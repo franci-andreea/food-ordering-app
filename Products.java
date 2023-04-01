f83db9a2e8a65aba330f86bf07c549707ed93673
gostmame <popescuadelin200@gmail.com>
2023-04-01 15:21:31 +0300

    update Products

--- Products.java
+++ Products.java
@@ -1,4 +1,25 @@
 public class Products {
     public String name;
     public Float price;
+
+    public Products(String name, Float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
 }
